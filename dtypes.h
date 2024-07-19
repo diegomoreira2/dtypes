e31ea4d961af4690d443d83fde822b9c546c076d
Diego Moreira <167494585+diegomoreira2@users.noreply.github.com>
2024-07-18 23:08:14 -0300

    Add files via upload
    Start.

---
+++ dtypes.h
@@ -0,0 +1,28 @@
+/*
+	Name:  DTypes
+	Version: 0.1
+	Date: 19/07/2024
+	Author: Diego Moreira
+*/
+
+// Typedefs simple for libs.
+// The naming follows the Rust language standard.
+
+#ifndef DTYPES_H
+#define DTYPES_H
+#include <stdint.h>
+#include <stdbool.h>
+
+	typedef bool i1;
+	typedef uint8_t u8;
+	typedef int8_t i8;
+	typedef uint16_t u16;
+	typedef int16_t i16;
+	typedef uint32_t u32;
+	typedef int32_t i32;
+	typedef uint64_t u64;
+	typedef int64_t i64;
+	typedef float f32;
+	typedef double f64;
+	
+#endif
